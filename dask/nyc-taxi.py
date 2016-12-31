92d94b1e572e59dfc60e8bfb2869fab02be6f1d8
dirkpetersen <petersen@fhcrc.org>
2016-12-30 17:09:17 -0800

    clean up nyc-taxi

--- dask/nyc-taxi.py
+++ dask/nyc-taxi.py
@@ -14,33 +14,11 @@ print(e)
 testdata = '/fh/scratch/delete30/_HDC/testdata'
 #testdata = '/fh/fast/_ADM/SciComp/data'
 
-#nyc0 = dd.read_csv(testdata+'/nyc-taxi-cleaned/yellow0/*.csv',
-#                parse_dates=['Trip_Pickup_DateTime', 'Trip_Dropoff_DateTime'])
-
-#nyc1 = dd.read_csv(testdata+'/nyc-taxi-cleaned/yellow1/*.csv',
-#        parse_dates=['pickup_datetime', 'dropoff_datetime'])
-             # skipinitialspace=True)
-
 nyc2 = dd.read_csv(testdata+'/nyc-taxi-cleaned/yellow2/*.csv',
         parse_dates=['tpep_pickup_datetime', 'tpep_dropoff_datetime'])
 
-
-#nyc0.head()
-#nyc1.head()
-#nyc2.head()
-
-#nyc0, nyc1, nyc2 = e.persist([nyc0, nyc1, nyc2])
-
 nyc2 = e.persist(nyc2)
 
-
-#print(nyc0.head())
-
-#print(nyc0.Payment_Type.value_counts().compute())
-#print(nyc1.payment_type.value_counts().compute())
-print('payment type:')
+print('payment type, please wait ...')
 print(nyc2.payment_type.value_counts().compute())
-
-
-#progress(nyc0, nyc1, nyc2)
 
